5be360dee5f913a5091d464fad85d7f1d8363ae1
whatphilipcodes <gerdes.philip@gmail.com>
2024-09-22 18:03:50 +0200

    msc: initial testing

--- beetsplug/fromregs.py
+++ beetsplug/fromregs.py
@@ -1,5 +1,11 @@
 from beets.plugins import BeetsPlugin
 
 
-class MyPlugin(BeetsPlugin):
-    pass
+class FromRegs(BeetsPlugin):
+    def __init__(self):
+        super(FromRegs, self).__init__()
+        self.register_listener("pluginload", self.loaded)
+
+    def loaded(self):
+        self._log.info("fromregs loaded!")
+        self._log.info(self.config["test"].get())
